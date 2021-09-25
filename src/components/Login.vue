0cfb4068a209a20fa8462101efe2ddd49c4e059d
AryaStar <757451920@qq.com>
2021-09-25 09:51:08 +0800

    添加全局样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,92 @@
 <template>
-  <div>
-    你好
+  <div class="login_container">
+    jin
+    <!-- <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="avatar" />
+      </div>
+      <div>
+        <el-form
+          ref="loginFormRef"
+          :model="loginForm"
+          :rules="loginFormRules"
+          label-width="60px"
+          class="login_form"
+        >
+          <el-form-item label="账号" prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              v-model="loginForm.password"
+              type="password"
+              prefix-icon="iconfont icon-3702mima"
+            ></el-input>
+          </el-form-item>
+          <el-form-item class="btns">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+export default {}
 </script>
 
 <style lang="less" scoped>
+/* // lang="less" 支持less格式
+// scoped vue的指令，只在当前组件生效 */
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+// .login_box {
+//   width: 450px;
+//   height: 360px;
+//   background-color: #fff;
+//   border-radius: 3px;
+//   position: absolute;
+//   left: 50%;
+//   top: 50%;
+//   -webkit-transform: translate(-50%, -50%);
+//   background-color: #fff;
+
+//   .avatar_box {
+//     width: 130px;
+//     height: 130px;
+//     border: 1px solid #eee;
+//     border-radius: 50%;
+//     padding: 10px;
+//     box-shadow: 0 0 10px #ddd;
+//     position: absolute;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//     background-color: #fff;
+//     img {
+//       width: 100%;
+//       height: 100%;
+//       border-radius: 50%;
+//       background-color: #eee;
+//     }
+//   }
+// }
+// .login_form {
+//   position: absolute;
+//   bottom: 60px;
+//   width: 100%;
+//   padding: 0 20px;
+//   box-sizing: border-box;
+// }
+// .btns {
+//   display: flex;
+//   justify-content: center;
+// }
+// .info {
+//   font-size: 13px;
+//   margin: 10px 15px;
+// }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-
+// import Login from '../components/Login.vue'
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
